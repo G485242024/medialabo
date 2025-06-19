56f2d3018d12265b8dbbed349a22601972011651
G485242024 <248524@st.takushoku-u.ac.jp>
2025-06-19 16:35:09 +0900

    第3回演習

--- kazuate.js
+++ kazuate.js
@@ -5,6 +5,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 // 入力回数（予想回数）
 let kaisu = 0;
 
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -17,8 +18,22 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+  kaisu++;
+  console.log(kaisu + "回目の予想: " + yoso);
+  if (kaisu >= 4) {
+    console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
+  } else if (yoso === kotae) {
+    console.log("正解です．おめでとう!");
+    kaisu = 4; // ゲーム終了のため強制的に4に
+  } else if (kaisu === 3) {
+    console.log("まちがい．残念でした答えは " + kotae + " です．");
+  } else if (yoso < kotae) {
+    console.log("まちがい．答えはもっと大きいですよ");
+  } else if (yoso > kotae) {
+    console.log("まちがい．答えはもっと小さいですよ");
+  }
 }
