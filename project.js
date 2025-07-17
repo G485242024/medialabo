93690fa03bc5caae0c9dbab8cb856bb9943fee14
G485242024 <248524@st.takushoku-u.ac.jp>
2025-07-17 16:05:57 +0900

    綴り間違い

--- project.js
+++
@@ -1,7 +0,0 @@
-function searchAction() {
-  let keyword = document.querySelector("#kyeword").value;
-  let resultBox = document.querySelector("#result");
-  resultBox.textContent = "「" + keyword + "」で検索しました";
-}
-let b = document.querySelector("button");
-b.addEventListener("click", searchAction);
